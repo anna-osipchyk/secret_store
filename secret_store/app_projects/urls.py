31575eadda075204a0aed81238f6bd33faaea66f
hanna <annaosipchik03@gmail.com>
2022-02-28 21:13:24 +0300

    added api for projects

--- secret_store/app_projects/urls.py
+++ secret_store/app_projects/urls.py
@@ -4,9 +4,6 @@ from .views import (
     MyViewedProjects,
     MySharedProjects,
     MyProjectVariables,
-    AddVariable,
-    DeleteVariable,
-    EditVariable,
 )
 
 urlpatterns = [
@@ -14,24 +11,10 @@ urlpatterns = [
     path("my_projects/<int:pk>/", MyProjects.as_view(), name="my_projects_put"),
     path("my_viewed_projects/", MyViewedProjects.as_view(), name="viewed_projects"),
     path("my_shared_projects/", MySharedProjects.as_view(), name="shared_projects"),
-    path("<int:fk>/variables/", MyProjectVariables.as_view(), name="variables"),
+    path("<int:pk>/variables/", MyProjectVariables.as_view(), name="variables"),
     path(
         "<int:fk>/variables/<int:pk>/",
         MyProjectVariables.as_view(),
         name="variables_put",
-    )
-    # path("my_project/<int:pk>/", MyProject.as_view(), name="my_project"),
-    # path("create/", CreateProject.as_view(), name="create_project"),
-    # path("delete/<int:pk>/", DeleteProject.as_view(), name="delete_project"),
-    # path("<int:pk>/add_variable/", AddVariable.as_view(), name="add_variable"),
-    # path(
-    #     "<int:project_id>/delete_variable/<int:pk>",
-    #     DeleteVariable.as_view(),
-    #     name="delete_variable",
-    # ),
-    # path(
-    #     "<int:project_id>/edit_variable/<int:pk>",
-    #     EditVariable.as_view(),
-    #     name="edit_variable",
-    # ),
+    ),
 ]
--- secret_store/app_projects/views.py
+++ secret_store/app_projects/views.py
@@ -1,26 +1,33 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.db.models import Q
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse_lazy
-from django.views.generic import (
-    ListView,
-    DetailView,
-    CreateView,
-    DeleteView,
-    UpdateView,
-)
+from rest_framework import permissions
+
+
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .serializers import ProjectSerializer, VariableSerializer
 from rest_framework.views import APIView
 
 from .models import ProjectModel, VariableModel
-from .forms import NewProjectForm, VariableForm
-from django.db import transaction
+
+
+class ViewPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        import pdb
+
+        pdb.set_trace()
+        user_id = request.user.id
+        project_id = request.data["my_projects"]["project"]
+        project = ProjectModel.objects.get(id=project_id)
+        is_viewer = project.viewers.contains(user_id)
+        return is_viewer
 
 
 class MyProjects(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         my_projects = ProjectModel.objects.filter(owner__id=request.user.id)
         print(my_projects)
@@ -49,6 +56,8 @@ class MyProjects(APIView):
 
 
 class MySharedProjects(APIView):
+    permission_classes = [IsAuthenticated, ViewPermission]
+
     def get(self, request):
         user_id = request.user.id
         shared_projects = ProjectModel.objects.prefetch_related("shared").filter(
@@ -59,6 +68,8 @@ class MySharedProjects(APIView):
 
 
 class MyViewedProjects(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         user_id = request.user.id
         viewed_projects = ProjectModel.objects.prefetch_related("viewers").filter(
@@ -69,6 +80,8 @@ class MyViewedProjects(APIView):
 
 
 class MyProjectVariables(APIView):
+    permission_classes = [ViewPermission, IsAuthenticated]
+
     def get(self, request, pk):
         project = get_object_or_404(ProjectModel, pk=pk)
         variables_of_project = VariableModel.objects.filter(project__id=project.id)
@@ -100,49 +113,29 @@ class MyProjectVariables(APIView):
         return Response({"OK": "Deleted"})
 
 
-class AddVariable(LoginRequiredMixin, CreateView):
-    form_class = VariableForm
-    context_object_name = "variable"
-    success_url = reverse_lazy("my_projects")
-    template_name = "app_projects/variable_add.html"
+class MySharedProjectsVariables(APIView):
+    permission_classes = [IsAuthenticated]
 
-    def form_valid(self, form):
-        self.variable = form.save(commit=False)
-        project_id = self.kwargs.get("pk")
-        variable_name = form.cleaned_data["name"]
-        variables_of_project = VariableModel.objects.filter(
-            Q(project__id=project_id) & Q(name=variable_name)
-        )
-        if len(variables_of_project) == 0:
-            self.variable.project = ProjectModel.objects.get(id=project_id)
-            self.variable.save()
-            return HttpResponseRedirect(self.success_url)
-        return HttpResponse("Value with this name already exists in your project!")
+    def get(self, request, fk):
+        project = get_object_or_404(ProjectModel, pk=fk)
+        variables_of_project = VariableModel.objects.filter(project__id=project.id)
+        serializer = VariableSerializer(variables_of_project, many=True)
+        return Response({"variables": serializer.data})
+
+    def post(self, request, fk):
+        project = get_object_or_404(ProjectModel, pk=fk)
+        variables_of_project = request.data.get("shared_variables")
+        serializer = VariableSerializer(data=variables_of_project)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        return Response({"OK": "Created"})
 
 
-class DeleteVariable(LoginRequiredMixin, DeleteView):
-    template_name = "app_projects/variable_delete.html"
-    success_url = reverse_lazy("my_projects")
-    model = VariableModel
-    context_object_name = "variable"
+class MyViewedProjectsVariables(APIView):
+    permission_classes = [IsAuthenticated]
 
-
-class EditVariable(LoginRequiredMixin, UpdateView):
-    template_name = "app_projects/variable_edit.html"
-    success_url = reverse_lazy("my_projects")
-    model = VariableModel
-    form_class = VariableForm
-    context_object_name = "variable"
-
-    def form_valid(self, form):
-        self.variable = form.save(commit=False)
-        project_id = self.kwargs.get("project_id")
-        variable_name = form.cleaned_data["name"]
-        variables_of_project = VariableModel.objects.filter(
-            Q(project__id=project_id) & Q(name=variable_name)
-        )
-        if len(variables_of_project) == 0:
-            self.variable.project = ProjectModel.objects.get(id=project_id)
-            self.variable.save()
-            return HttpResponseRedirect(self.success_url)
-        return HttpResponse("Value with this name already exists in your project!")
+    def get(self, request, fk):
+        project = get_object_or_404(ProjectModel, pk=fk)
+        variables_of_project = VariableModel.objects.filter(project__id=project.id)
+        serializer = VariableSerializer(variables_of_project, many=True)
+        return Response({"variables": serializer.data})
